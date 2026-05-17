d6c9881a4b1ceb2ecd9d0b96f5252430224dc118
Ahlam_Fouad <fouadahlam37@gmail.com>
2026-05-17 16:17:37 +0100

    initial commit

--- pages/views.py
+++ pages/views.py
@@ -8,3 +8,11 @@ from django.http import HttpResponse
 
 def home_page_view(request):
     return HttpResponse("Homepage")
+
+
+def about_page_view(request):
+    context = {"name": "Ahlam",
+               "age": 26,
+               }
+
+    return render(request, "pages/about.html", context)  # new
